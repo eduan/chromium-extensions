29e6b81d10d99b18ba09487934fd3f1f6925d4ef
Eduan Lenine <eduanlenine@yahoo.com.br>
2012-02-26 02:51:15 -0300

    Corrigindo regex ticket

--- app-vale-refeicao/js/TicketSource.js
+++ app-vale-refeicao/js/TicketSource.js
@@ -5,7 +5,7 @@ function TicketSource(){
 			url:"http://www.ticket.com.br/portal/portalcorporativo/dpages/service/consulteseusaldo/seeBalance.jsp?chkProduto=Ticket+Restaurante&txtNumeroCartao=" + localStorage['numero'] + "&txtOperacao=saldo_agendamentos&cardNumber=&Consultar.x=908&Consultar.y=368",
 			dataType:"html",
 			success:function(data){
-				preencherSaldo(data.match(/((\b[0-9]+)?\,)[0-9]+\b/g).toString());
+				preencherSaldo(data.match(/[\d]{1,},[\d]{1,}/).toString());
 			 	$("#informacoesSaldo").show();
 			}
 		})
