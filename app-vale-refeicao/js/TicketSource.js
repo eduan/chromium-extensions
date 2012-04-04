354b87ac130d8a77897c3d25b24fae539ee5b9d4
Eduan Lenine <eduan.lenine@jacotei.com.br>
2012-04-04 14:58:09 -0300

    Atualizando url de acesso do Ticket

--- app-vale-refeicao/js/TicketSource.js
+++ app-vale-refeicao/js/TicketSource.js
@@ -2,7 +2,7 @@ function TicketSource(){
 
 	this.configurarSaldo = function(){
 		$.ajax({
-			url:"http://www.ticket.com.br/portal/portalcorporativo/dpages/service/consulteseusaldo/seeBalance.jsp?chkProduto=Ticket+Restaurante&txtNumeroCartao=" + localStorage['numero'] + "&txtOperacao=saldo_agendamentos&cardNumber=&Consultar.x=908&Consultar.y=368",
+			url:"http://www.ticket.com.br/consultcard-web/consultCardGet?chkProduto=Ticket+Restaurante&txtNumeroCartao=" + localStorage['numero'] + "&txtOperacao=saldo_agendamentos&cardNumber=",
 			dataType:"html",
 			success:function(data){
 				preencherSaldo(data.match(/[\d]{1,},[\d]{1,}/).toString());
