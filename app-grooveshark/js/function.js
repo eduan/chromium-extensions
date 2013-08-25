0436d32781c254f059c05466b339bbc8f0959043
Eduan Lenine <eduanlenine@yahoo.com.br>
2013-08-25 19:07:05 -0300

    Add style

--- app-grooveshark/js/function.js
+++ app-grooveshark/js/function.js
@@ -4,8 +4,10 @@ var lista = new Array();
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if ((tab != null) && (tab.url.search("grooveshark.com") >= 0)) {
     groovesharkTab = tabId;
+    $('#groovesharck-status').text(' v Aberto');
     chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
       if (tabId == groovesharkTab)
+        $('#groovesharck-status').text('x Fechado');
         groovesharkTab = -1;
     });
   }
@@ -13,6 +15,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 chrome.tabs.getSelected(null, function(tab){
     if(tab.url.search('grooveshark.com')){
+        $('#groovesharck-status').text('v Aberto');
         groovesharkTab = tab.id;
     }
 });
@@ -26,6 +29,13 @@ $(function() {
     });
 
     $(".player").click(function(){
+        
+        $(".msg").hide();
+
+        if(!lista.length){
+          $(".msg").fadeIn('slow');
+        }
+
         for(var i=0; i < lista.length; i++){
             chrome.tabs.executeScript(groovesharkTab, { code: 
                 "setTimeout(\"" +
