cf0d170418cf04da536fea2cca6bb4bf80373e46
Eduan Lenine <eduanlenine@yahoo.com.br>
2013-09-27 23:11:43 -0300

    Finalizando prototipo

--- app-facebook-anywhere/js/function.js
+++ app-facebook-anywhere/js/function.js
@@ -5,10 +5,36 @@
 //   js.src = "https://connect.facebook.net/en_US/all.js#xfbml=1&appId=393816267411515";
 //   fjs.parentNode.insertBefore(js, fjs);
 // }(document, 'script', 'facebook-jssdk'));
-var comments = '<div style="position: fixed; top: 80%; height: 230px; width: 560px; margin-left:-275px; left: 50%; background-color: white; z-index: 100000000000; overflow: auto !important;"><iframe id="f3bafd5014" name="f2a2e64b7c" scrolling="no" title="Facebook Social Plugin" class="fb_ltr" src="https://www.facebook.com/plugins/comments.php?api_key=113869198637480&amp;channel_url=https%3A%2F%2Fs-static.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D27%23cb%3Df27009498%26domain%3Ddevelopers.facebook.com%26origin%3Dhttps%253A%252F%252Fdevelopers.facebook.com%252Ff3490537a4%26relation%3Dparent.parent&amp;href=http%3A%2F%2Fg1.globo.com&amp;locale=en_US&amp;numposts=3&amp;sdk=joey&amp;width=470" style="border: none; overflow: hidden; height: 200px; width: 550px;"></iframe></div>';
+function initIframe(url){
+    return  '<div id="facebook_comment_anywhere" style="position: fixed; top: 60%; height: 400px; width: 560px; margin-left:-275px; left: 50%; background-color: white; z-index: 100000000000; overflow: scroll !important; border: 1px solid #ccc;">' +
+                '<a style="float:right; font-family: \\\'lucida grande\\\',tahoma,verdana,arial,sans-serif; font-size: 11px;" href="#" onclick="document.getElementById(\\\'facebook_comment_anywhere\\\').remove()">[x] Fechar</a>' +
+                '<iframe' +
+                    ' name="facebook_comment_anywhere"' +
+                    ' scrolling="yes"' +
+                    ' src="https://www.facebook.com/plugins/comments.php?api_key=113869198637480&amp;href=' + url + '&amp;locale=en_US&amp;numposts=3&amp;sdk=joey&amp;width=450"' +
+                    ' style="border: none; overflow-x: hidden; height: 300px; width: 550px;"> </iframe>' + 
+            '</div>';
+}
 
-chrome.tabs.getCurrent(function(tab){
-	chrome.tabs.executeScript(tab.id, { code: 
-	    "document.body.innerHTML += '" + comments + "';";
-	});	
+var tabId = -1;
+var tabUrl = '';
+
+chrome.tabs.getSelected(null, function(tab){ 
+    tabId = tab.id ;
+    tabUrl = escape(tab.url);
+    if(tabId){
+        chrome.tabs.executeScript(tabId, { code: 
+            "if(document.getElementById('facebook_comment_anywhere')){ " + 
+            "   document.getElementById('facebook_comment_anywhere').remove() " +
+            "}" + 
+            "document.body.innerHTML += '" + initIframe(tabUrl) + "';"
+        });    
+    }
+
+    $.getJSON('https://graph.facebook.com/?ids=' + tabUrl, function(data){
+        if(data.length){
+            chrome.browserAction.setBadgeText({text: data[tabUrl]['comments']})
+        }
+    });
 })
+
